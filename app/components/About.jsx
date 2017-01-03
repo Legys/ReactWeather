36b97dad3d5dae13740c15f4c278524b2d1bd0f2
Alexandr Dubrovin <legys5@yandex.ru>
2017-01-03 18:39:00 +0200

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,12 +1,18 @@
 var React = require('react');
-
-$('p')
+var {Link} = require('react-router');
 
 var About = (props) => {
     return (
             <div>
-                <h3>About</h3>
-                <p>Welcome to About</p>
+                <h1 className="text-center">About</h1>
+                <p>
+                    This application serve to get weather in any country what you type in.
+                </p>
+                <div className="row">
+                    <div className="text-center large-6 small-10">
+                        <Link to="/examples"> Look for examples here >></Link>
+                    </div>
+                </div>
             </div>
     )
 };
